8b0a23a5efaaf13a9f71598462ec148421394967
Abeille1990 <tatiana.tyukova@gmail.com>
2024-07-04 13:37:10 +0300

    HW_Variables

---
+++ Variables.py
@@ -0,0 +1,5 @@
+Number_of_passed_tasks = 12
+Spent_hours = 1.5
+Course_name = 'Python'
+Time_per_task = Number_of_passed_tasks / Spent_hours
+print('Курс: ',Course_name,", всего задач:",Number_of_passed_tasks,", затрачено часов: ",Spent_hours,", среднее время выполнения ",str(Spent_hours / Number_of_passed_tasks))
