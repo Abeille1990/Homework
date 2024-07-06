b713da18aa021a41559e3fe1bb1bba204b488b83
Abeille1990 <tatiana.tyukova@gmail.com>
2024-07-06 23:27:40 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+# Program org
+my_string = input('Tell me what is your mood today: ')
+print(len(my_string))
+# string methods
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
