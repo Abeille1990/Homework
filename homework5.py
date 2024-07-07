d03151a52731134da66985c3b5c2cade314f0ff9
Abeille1990 <tatiana.tyukova@gmail.com>
2024-07-07 13:56:22 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+tuple_ = 1,2,4,5
+print('Immutable_var:',tuple_)
+#tuple_[0]=784 - - попыталась изменить часть кортежа, не знаю как это еще показать, кроме как создать комментарий тут =)
+#print(tuple_)
+list_= [1,2,4,5]
+print('Mutable_var:',list_)
+list_[2]=3
+list_[3]=4
+print('Mutable_var (Modified):',list_)
