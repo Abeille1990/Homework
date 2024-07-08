770d9ec804e70243d9f91cde3af12a133dbf395d
Abeille1990 <tatiana.tyukova@gmail.com>
2024-07-08 11:08:24 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -2,6 +2,7 @@ name = "Tanya"
 print('Name: ',name)
 Age = 34
 print('Age: ',Age)
-print('New age: ',Age+3)
+Age = Age + 3
+print('New age: ',Age)
 is_student = True
 print('Is student: ',is_student)
