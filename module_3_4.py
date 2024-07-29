626bd31638417f5da5e9225217db369e8934e5cc
Abeille1990 <tatiana.tyukova@gmail.com>
2024-07-30 00:37:05 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -3,8 +3,8 @@ def single_root_words (root_word, *other_words):
     root_word_upper = root_word.upper()
 
     for i in other_words: # перебираем кажое слово в множестве Other_words и как бы определяем каждую единицу переменной i
-        i.upper() # переводим все слова в верхний регистр для игнорирования регистра
-        if root_word_upper in i.upper() or i.upper() in root_word_upper: # выполняем поиск слов, которые или содержат
+        other_words_upper = i.upper() # переводим все слова в верхний регистр для игнорирования регистра
+        if root_word_upper in other_words_upper or other_words_upper in root_word_upper: # выполняем поиск слов, которые или содержат
             # коренное слово содержит в себе слово из списка
             same_words.append(i)
 
